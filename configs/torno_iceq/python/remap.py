4e8e269cc0225ee50262240a0b05c656d2068255
iceqhub <stocksoftwaresdev@gmail.com>
2026-05-01 12:21:19 -0300

    att NOVO PACOTE de att para troca de ferramenta

--- configs/torno_iceq/python/remap.py
+++ configs/torno_iceq/python/remap.py
@@ -7,6 +7,9 @@ TORRE_FILE = os.path.join(
     "torre.txt"
 )
 
+# Atributo Python para rastrear posição da torre
+# Separado do self.params para garantir persistência correta
+
 def init_stdglue(self):
     self.sticky_params = dict()
     if self.task:
@@ -14,21 +17,28 @@ def init_stdglue(self):
             with open(TORRE_FILE, "r") as f:
                 val = int(f.read().strip())
                 if 1 <= val <= 8:
-                    self.params["_torre"] = float(val)
+                    self._torre_pos = val
                 else:
-                    self.params["_torre"] = 1.0
+                    self._torre_pos = 1
         except:
-            self.params["_torre"] = 1.0
-        print("Torre carregada: posicao %d" % int(self.params["_torre"]))
+            self._torre_pos = 1
+        # Injeta no params para o NGC ler
+        self.params["_torre"] = float(self._torre_pos)
+        print("Torre carregada: posicao %d" % self._torre_pos)
 
-def salva_torre(self):
-    try:
-        pos = int(self.params["_torre"])
-        with open(TORRE_FILE, "w") as f:
-            f.write(str(pos))
-        print("Torre salva: posicao %d" % pos)
-    except Exception as e:
-        print("Erro ao salvar torre: %s" % e)
+def salva_torre(self, **words):
+    """Chamado via REMAP M500 pelo .m6.ngc após cada troca"""
+    if self.task:
+        try:
+            # Lê posição atual do params (atualizado pelo NGC)
+            pos = int(self.params["_torre"])
+            self._torre_pos = pos
+            with open(TORRE_FILE, "w") as f:
+                f.write(str(pos))
+            print("Torre salva: posicao %d" % pos)
+        except Exception as e:
+            print("Erro ao salvar torre: %s" % e)
+    return INTERP_OK
 
 def m400(self, *args):
     data = float(datetime.datetime.now().strftime("%m%d%Y%H%M"))
--- configs/torno_iceq/python/toplevel.py
+++ configs/torno_iceq/python/toplevel.py
@@ -5,6 +5,12 @@ def __init__(self):
     remap.init_stdglue(self)
 
 def __delete__(self):
-    # Salva posição da torre apenas na instância milltask (não no preview)
     if self.task:
-        remap.salva_torre(self)
+        # Salvamento final ao fechar
+        try:
+            pos = int(self.params["_torre"])
+            with open(remap.TORRE_FILE, "w") as f:
+                f.write(str(pos))
+            print("Torre salva ao fechar: posicao %d" % pos)
+        except Exception as e:
+            print("Erro ao fechar torre: %s" % e)
